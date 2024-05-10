25b161ce46926f0631e9e46ab1c63fd8c5997730
NasimYousefi <nasim.yousefi05@gmail.com>
2024-05-10 14:18:26 +0330

    normalaize rss data

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getRssResult } from './rss.js'
 
 const rssFeedsUrls =[
     'https://bookriot.com/feed/',
-'https://www.kirkusreviews.com/feeds/rss/',
+
 ]
 
 async function getRssFeeds(feedsUrl){
@@ -13,11 +13,28 @@ requests.push(getRssResult(url))
     });
 
     const feeds = await Promise.all(requests);
-    console.log(feeds);
+    // console.log(feeds);
 
- 
-
+    const newsLetter =[]
+    feeds.forEach(feed =>{
+        feed.items.forEach(item => {
+            newsLetter.push({
+                title: item.title,
+                creator:item.creator,
+                link:item.link,
+                pubDate:item.pubDate,
+                provider:{
+                    name:feed.title,
+                    link:feed.link,
+                    language:feed.language,
+                    lastBuildDate:feed.lastBuildDate,
+                }
+            })
+        })
+    })
+return newsLetter
 }
 
 getRssFeeds(rssFeedsUrls)
+.then(data => console.log(data))
 .catch(error=>console.error(error))
