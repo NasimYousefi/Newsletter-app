fafa21cdf10f07c8cba9be56cb029402881ed610
NasimYousefi <nasim.yousefi05@gmail.com>
2024-05-10 16:52:20 +0330

    update app to send email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import { getRssResult } from './rss.js'
+// import { sendTextMail } from './mail.js'
+import { sendHtmlMail } from './mail.js'
 
 const rssFeedsUrls =[
     'https://bookriot.com/feed/',
@@ -35,6 +37,44 @@ requests.push(getRssResult(url))
 return newsLetter
 }
 
-getRssFeeds(rssFeedsUrls)
-.then(data => console.log(data))
-.catch(error=>console.error(error))
+async function sendNewsLetter(){
+    try {
+        const feeds = await getRssFeeds(rssFeedsUrls);
+        const newsBody = feeds.map(item =>{
+            return`
+            <div style="background-color: #f4f4f4; padding: 20px; border-radius: 10px;">
+        <h2 style="margin-bottom: 10px;">${item.title}</h2>
+        <p style="margin-bottom: 10px;">Creator: ${item.creator}</p>
+        <p style="margin-bottom: 10px;">Publication Date: ${item.pubDate}</p>
+        <p style="margin-bottom: 10px;">Provider: ${item.provider.name}</p>
+        <p style="margin-bottom: 10px;">Provider Link: <a href="${item.provider.link}">${item.provider.link}</a></p>
+        <p style="margin-bottom: 10px;">Provider Language: ${item.provider.language}</p>
+        <p style="margin-bottom: 10px;">Provider Last Build Date: ${item.provider.lastBuildDate}</p>
+        <p style="margin-bottom: 10px;">Read more: <a href="${item.link}">${item.link}</a></p>
+    </div>
+            `
+        })
+        let emailHtml = `<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px;">${newsBody}</div>`;
+        const mailRecipints = ['nasim.yousefi05@gmail.com']
+        const mailResponse = await sendHtmlMail(mailRecipints,'newsLetter',emailHtml)
+        return mailResponse;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+sendNewsLetter()
+.then(response => console.log(response))
+.catch(error => console.error(error))
+
+// getRssFeeds(rssFeedsUrls)
+// .then(data => console.log(data))
+// .catch(error=>console.error(error))
+
+
+
+// sendTextMail(mailRecipints,'newsLetter','Hello Nasim')
+// .then(data => console.log(data))
+// .catch(error=>console.error(error))
+
