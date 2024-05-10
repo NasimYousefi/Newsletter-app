59b97fa59fee7c3d1292faccea3873c31ad0c9cb
NasimYousefi <nasim.yousefi05@gmail.com>
2024-05-10 13:05:40 +0330

    get rss

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 import 'dotenv/config'
+import { getRssResult } from './rss.js'
+
+const rssFeedsUrls =[
+    'https://bookriot.com/feed/',
+'https://www.kirkusreviews.com/feeds/rss/',
+]
+
+async function getRssFeeds(feedsUrl){
+   const requests = []
+    feedsUrl.forEach(url=>{
+requests.push(getRssResult(url))
+    });
+
+    const feeds = await Promise.all(requests);
+    console.log(feeds);
+
+ 
+
+}
+
+getRssFeeds(rssFeedsUrls)
+.catch(error=>console.error(error))
