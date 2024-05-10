9b029886c55abc52e602ea375ffe7f5dafde10b7
NasimYousefi <nasim.yousefi05@gmail.com>
2024-05-10 16:52:01 +0330

    add secret file

---
+++ sec.js
@@ -0,0 +1,7 @@
+export const mailSecrets = {
+    host:process.env.MAIL_PROVIDER_HOST,
+    port:process.env.MAIL_PROVIDER_PORT,
+    user:process.env.MAIL_PROVIDER_USER,
+    password:process.env.MAIL_PROVIDER_PASSWORD,
+    defult_sender:process.env.DEFULT_MAIL_SENDER,
+}
